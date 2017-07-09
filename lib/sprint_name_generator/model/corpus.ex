17a06c383cbd00eccde9a65986470fbdaae4ae59
Trevor Strieber <trevor@strieber.org>
2017-07-09 16:54:09 -0700

    Add a couple helper functions to Corpus.

--- lib/sprint_name_generator/model/corpus.ex
+++ lib/sprint_name_generator/model/corpus.ex
@@ -1,11 +1,24 @@
 defmodule SprintNameGenerator.Model.Corpus do
   use Ecto.Schema
 
+  import Ecto.Query, only: [from: 2]
+
+  alias Markov.Dictionary
+  alias SprintNameGenerator.Model.Corpus
+
   schema "corpora" do
     field :name, :string
     field :text, :string
     field :markov, :map
 
     timestamps()
+  end
+
+  def find_by_name(name) do
+    from Corpus, where: [name: ^name]
+  end
+
+  def from(%Dictionary{name: name, text: text, markov: markov}) do
+    %Corpus{name: name, text: text, markov: markov}
   end
 end
