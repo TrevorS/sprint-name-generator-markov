07bb5cac9f8e6274438ca69aae866ed7c1ae8dee
Trevor Strieber <trevor@strieber.org>
2017-07-09 12:40:44 -0700

    Dictionary is now a struct.

--- lib/markov/dictionary.ex
+++ lib/markov/dictionary.ex
@@ -1,14 +1,25 @@
+alias Markov.Dictionary
+
 defmodule Markov.Dictionary do
   @split_regex ~r/\s+|\.|\?|,|;|:|-/
+  @enforce_keys [:text, :markov]
 
-  def generate(text) do
-    @split_regex
-      |> Regex.split(text, trim: true)
-      |> Enum.chunk(2, 1)
-      |> Enum.reduce(%{}, &add_to_map/2)
+  defstruct [:name, :text, :markov]
+
+  def new(text, opts \\ []) do
+    markov = generate_markov(text)
+
+    %Dictionary{text: text, markov: markov, name: opts[:name]}
   end
 
-  def add_to_map([word, next_word], word_map) do
+  defp generate_markov(text) do
+    @split_regex
+    |> Regex.split(text, trim: true)
+    |> Enum.chunk(2, 1)
+    |> Enum.reduce(%{}, &add_to_map/2)
+  end
+
+  defp add_to_map([word, next_word], word_map) do
     Map.update(word_map, word, [next_word], fn(words) ->
       [next_word | words] |> Enum.uniq
     end)
