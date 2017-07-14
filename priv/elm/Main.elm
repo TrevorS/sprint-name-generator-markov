d7f53f5ef63c23c35375e9b61160ac42e23cf985
Trevor Strieber <trevor@strieber.org>
2017-07-14 07:50:32 -0700

    WIP

--- priv/elm/Main.elm
+++ priv/elm/Main.elm
@@ -1,4 +1,32 @@
-import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
 
-main = span [class "welcome"] [text "Hello World"]
+main = Html.beginnerProgram { model = model, update = update, view = view }
+
+-- MODEL
+type alias Model = String
+
+model : Model
+model =
+  ""
+
+-- UPDATE
+type Msg = GetSprintName | ClearSprintName
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    GetSprintName ->
+      model ++ "did it "
+
+    ClearSprintName ->
+      ""
+
+-- VIEW
+view : Model -> Html Msg
+view model =
+  div []
+    [ button [ onClick GetSprintName ] [ text "Get Sprint Name" ]
+    , button [ onClick ClearSprintName ] [ text "Clear" ]
+    , div [] [ text model ]
+    ]
