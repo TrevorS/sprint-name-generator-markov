28ed05f46343f888d9d020d470af1094f2f8df56
Trevor Strieber <trevor@strieber.org>
2017-08-07 20:29:12 -0700

    Split out view

--- priv/elm/Main.elm
+++ priv/elm/Main.elm
@@ -1,12 +1,12 @@
-import Html exposing (Html, button, div, input, text, ul, li)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onClick, onInput)
+import Html exposing(Html)
 
 import Model exposing (Model, model)
+import View exposing(view)
+
 import Corpus exposing (Corpus)
-import SelectCorpora exposing (selectCorpora)
+
 import Msgs exposing (Msg)
-import Server exposing (..)
+import Server exposing (getRandomSprintName, getSprintName, getCorpora, submitCorpus)
 
 main : Program Never Model Msg
 main =
@@ -125,60 +125,3 @@ update msg model =
       Debug.crash("error: " ++ (toString error))
 
       (model, Cmd.none)
-
--- VIEW
-view : Model -> Html Msg
-view model =
-  div []
-    [ viewSprintName model.sprintName
-    , viewSprintNameButtons model.selectedCorpus
-    , selectCorpora model.corpora
-    , viewNewCorpusNameInput
-    , viewNewCorpusTextInput
-    , viewNewCorpusSubmitButtons
-    , viewErrors model.errorMessage
-    ]
-
-viewSprintName : List String -> Html Msg
-viewSprintName sprintName =
-  ul []
-    (List.map (\word -> li [] [ text word ])
-      sprintName)
-
-viewSprintNameButtons : String -> Html Msg
-viewSprintNameButtons selectedCorpus =
-  div []
-    [ button [ onClick (Msgs.GetSprintName selectedCorpus) ] [ text "Get Sprint Name" ]
-    , button [ onClick Msgs.ClearSprintName ] [ text "Clear" ]
-    ]
-
-viewNewCorpusNameInput : Html Msg
-viewNewCorpusNameInput =
-  div []
-    [ input
-      [ placeholder "New Corpus Name"
-      , onInput Msgs.ChangeNewCorpusNameInput
-      ]
-      []
-    ]
-
-viewNewCorpusTextInput : Html Msg
-viewNewCorpusTextInput =
-  div []
-    [ input
-      [ placeholder "New Corpus Text"
-      , onInput Msgs.ChangeNewCorpusTextInput
-      ]
-      []
-    ]
-
-viewNewCorpusSubmitButtons : Html Msg
-viewNewCorpusSubmitButtons =
-  div []
-    [ button [ onClick Msgs.SubmitCorpus ] [ text "Submit Corpus" ]
-    , button [ onClick Msgs.ClearCorpus ] [ text "Clear Corpus" ]
-    ]
-
-viewErrors : String -> Html Msg
-viewErrors errorMessage =
-  div [] [ text errorMessage ]
---
+++ priv/elm/View.elm
@@ -0,0 +1,65 @@
+module View exposing (view)
+
+import Html exposing (Html, button, div, input, text, ul, li)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
+
+import Model exposing(Model)
+import Msgs exposing (Msg)
+import SelectCorpora exposing(selectCorpora)
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ viewSprintName model.sprintName
+    , viewSprintNameButtons model.selectedCorpus
+    , selectCorpora model.corpora
+    , viewNewCorpusNameInput
+    , viewNewCorpusTextInput
+    , viewNewCorpusSubmitButtons
+    , viewErrors model.errorMessage
+    ]
+
+viewSprintName : List String -> Html Msg
+viewSprintName sprintName =
+  ul []
+    (List.map (\word -> li [] [ text word ])
+      sprintName)
+
+viewSprintNameButtons : String -> Html Msg
+viewSprintNameButtons selectedCorpus =
+  div []
+    [ button [ onClick (Msgs.GetSprintName selectedCorpus) ] [ text "Get Sprint Name" ]
+    , button [ onClick Msgs.ClearSprintName ] [ text "Clear" ]
+    ]
+
+viewNewCorpusNameInput : Html Msg
+viewNewCorpusNameInput =
+  div []
+    [ input
+      [ placeholder "New Corpus Name"
+      , onInput Msgs.ChangeNewCorpusNameInput
+      ]
+      []
+    ]
+
+viewNewCorpusTextInput : Html Msg
+viewNewCorpusTextInput =
+  div []
+    [ input
+      [ placeholder "New Corpus Text"
+      , onInput Msgs.ChangeNewCorpusTextInput
+      ]
+      []
+    ]
+
+viewNewCorpusSubmitButtons : Html Msg
+viewNewCorpusSubmitButtons =
+  div []
+    [ button [ onClick Msgs.SubmitCorpus ] [ text "Submit Corpus" ]
+    , button [ onClick Msgs.ClearCorpus ] [ text "Clear Corpus" ]
+    ]
+
+viewErrors : String -> Html Msg
+viewErrors errorMessage =
+  div [] [ text errorMessage ]
